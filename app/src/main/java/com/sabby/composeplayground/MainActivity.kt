d4d263f9fe7ab229d999a81ac41c00b5a67d2ebc
DANGEROUSMACHIN\user <login.inderpreet@gmail.com>
2021-10-26 21:23:27 +0530

    -text styling

--- app/src/main/java/com/sabby/composeplayground/MainActivity.kt
+++ app/src/main/java/com/sabby/composeplayground/MainActivity.kt
@@ -3,21 +3,32 @@ package com.sabby.composeplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.sabby.composeplayground.ui.theme.ComposePlaygroundTheme
 
 class MainActivity : ComponentActivity() {
+    private val fontFamily = FontFamily(
+        Font(R.font.montserrat_regular),
+        Font(R.font.montserrat_bold, FontWeight.Bold),
+        Font(R.font.montserrat_medium, FontWeight.Medium)
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             ComposePlaygroundTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                Column {
+                    Greeting("Compose Playground")
+                    TextWithFontFamily(name = "Text with different font", fontFamily)
                 }
             }
         }
@@ -26,7 +37,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "$name!", color = Color.Cyan, fontSize = 20.sp)
+}
+
+@Composable
+fun TextWithFontFamily(name: String, fontFamily: FontFamily) {
+    Text(text = "$name!", color = Color.Red, fontFamily = fontFamily, fontWeight = FontWeight.Bold)
 }
 
 @Preview(showBackground = true)
@@ -34,5 +50,12 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     ComposePlaygroundTheme {
         Greeting("Android")
+        TextWithFontFamily(
+            "Android", fontFamily = FontFamily(
+                Font(R.font.montserrat_regular),
+                Font(R.font.montserrat_bold, FontWeight.Bold),
+                Font(R.font.montserrat_medium, FontWeight.Medium)
+            )
+        )
     }
 }
